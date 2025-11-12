342dd2adfa7824c3d525049c1cd7117e487f77b9
FaridaTamer16007220 <farida.nada@student.giu-uni.de>
2025-11-12 14:55:16 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -16,23 +16,7 @@ bool isHungry;
 public:
 void display();
 void feed();
-
-
-  class Mammal:public Animal{
-    
-    string furColor;
-    
-  }
-  
-  class Bird:public Animal{
-    float wingSpan;
-    
-  }
-  
-  class Reptile:public Animal{
-    
-    bool isVenomous;
-  }
 };
+
 
 #endif

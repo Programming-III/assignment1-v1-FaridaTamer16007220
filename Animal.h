5779184f9ed82d75e693ec622f6f7dee1a7978c4
FaridaTamer16007220 <farida.nada@student.giu-uni.de>
2025-11-12 14:54:15 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,38 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
-
-#include <string>
+#include <iostream>
+#include<string>
 using namespace std;
 
-//define animal class here 
+
+class Animal{
+  
+private:
+string name;
+int age;
+bool isHungry;
 
 
+public:
+void display();
+void feed();
+
+
+  class Mammal:public Animal{
+    
+    string furColor;
+    
+  }
+  
+  class Bird:public Animal{
+    float wingSpan;
+    
+  }
+  
+  class Reptile:public Animal{
+    
+    bool isVenomous;
+  }
+};
 
 #endif
